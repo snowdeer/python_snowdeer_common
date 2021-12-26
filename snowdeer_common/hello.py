db687ff1e89f5fd38800281e3bf54bc351a59734
snowdeer <snowdeer0314@gmail.com>
2021-12-26 15:16:34 +0900

    init commit

--- snowdeer_common/hello.py
+++ snowdeer_common/hello.py
@@ -1,2 +1,10 @@
 def hello_snowdeer():
     print("Hello, snowdeer !!")
+
+
+class SnowDeer:
+    def __init__(self):
+        print("SnowDeer::init()")
+
+    def hello(self):
+        print("Snowdeer::hello()")
