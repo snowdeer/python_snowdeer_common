cc26b0899648e51c8b184d8f18144babd7095674
snowdeer <snowdeer0314@gmail.com>
2021-12-26 14:45:46 +0900

    init commit

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='snowdeer_common',
+setup(name='python_snowdeer_common',
       version='0.1.0',
       description='snowdeer sample',
       url='https://snowdeer.github.io',
       author='snowdeer',
       author_email='snowdeer0314@gmail.com',
       license='Apache',
-      packages=['snowdeer_common'],
+      packages=['python_snowdeer_common'],
       zip_safe=False)
